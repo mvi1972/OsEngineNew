51943d5d739b411e89c8a5cc2bc4dd786f37bde3
mvi1972 <61858922+mvi1972@users.noreply.github.com>
2022-03-23 14:48:09 +0300

    Создал ветку разработки и балванку робота Скальпер

---
+++ OsEngine/Robots/Scalper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using OsEngine.Charts.CandleChart.Indicators;
+using OsEngine.Entity;
+using OsEngine.Market;
+using OsEngine.OsTrader.Panels;
+using OsEngine.OsTrader.Panels.Tab;
+
+
+namespace OsEngine.Robots
+{
+    public class Scalper : BotPanel
+    {
+        public Scalper(string name, StartProgram startProgram) : base(name, startProgram)
+        {
+
+        }
+        public override string GetNameStrategyType()
+        {
+            return "Scalper";
+        }
+
+        public override void ShowIndividualSettingsDialog()
+        {
+           
+        }
+    }
+}
