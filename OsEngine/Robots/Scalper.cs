0707afe90ab5f61269fa59d9a25acc897bf23a99
mvi1972 <61858922+mvi1972@users.noreply.github.com>
2022-03-25 11:53:43 +0300

    начал создание инструкторы

--- OsEngine/Robots/Scalper.cs
+++ OsEngine/Robots/Scalper.cs
@@ -12,18 +12,37 @@ namespace OsEngine.Robots
 {
     public class Scalper : BotPanel
     {
+        /// <summary>
+        /// конструктор 
+        /// </summary>
         public Scalper(string name, StartProgram startProgram) : base(name, startProgram)
         {
 
         }
+
+        /// <summary>
+        /// вернуть название робота
+        /// </summary>
         public override string GetNameStrategyType()
         {
             return "Scalper";
         }
 
+        /// <summary>
+        /// показать окно индивидуальных настроек 
+        /// </summary>
         public override void ShowIndividualSettingsDialog()
         {
            
+        }
+
+        /// <summary>
+        /// проверить условия на вход в позицию
+        /// </summary>
+        private void TryOpenPosition(List<Candle> candles)
+        {
+            decimal lastPrice = candles[candles.Count - 1].Close;
+            // проверка индикатора
         }
     }
 }
