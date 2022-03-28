1dabd6eb6b4fa388350cc9a66ef96cd1270e3012
mvi1972 <61858922+mvi1972@users.noreply.github.com>
2022-03-28 11:50:51 +0300

    создал вкладку

--- OsEngine/Robots/Scalper.cs
+++ OsEngine/Robots/Scalper.cs
@@ -12,13 +12,71 @@ namespace OsEngine.Robots
 {
     public class Scalper : BotPanel
     {
+        // публичные настройки
+        /// <summary>
+        /// вкл/выкл для робота
+        /// </summary>
+        public bool IsOn;
+
+        /// <summary>
+        /// вкл/выкл для бумаги
+        /// </summary>
+        public bool IsOnPapir;
+
+        // публичные переменные 
+        /// <summary>
+        /// цена начала фазы роста
+        /// </summary>
+        public decimal PriceGrowthPhase;
+
+        /// <summary>
+        /// вкладка через которую робот совершает торговлю
+        /// </summary>
+        private BotTabSimple _tab;
+
+
         /// <summary>
         /// конструктор 
         /// </summary>
         public Scalper(string name, StartProgram startProgram) : base(name, startProgram)
         {
 
+            TabCreate(BotTabType.Simple);
+            _tab = TabsSimple[0];
+            _tab.CandleFinishedEvent += _tab_CandleFinishedEvent;
         }
+
+        /// <summary>
+        /// событие завершения новой свечи
+        /// </summary>
+        private void _tab_CandleFinishedEvent(List<Candle> candles)
+        {
+            
+        }
+
+        // логика 
+        /// <summary>
+        /// расчет точки роста
+        /// </summary>
+        private void СalculationPointGrowth(List<Candle> candles)
+        {
+
+        }
+
+        /// <summary>
+        /// проверить условия на вход в позицию
+        /// </summary>
+        private void TryOpenPosition(List<Candle> candles)
+        {
+            decimal lastPrice = candles[candles.Count - 1].Close;
+            // проверка индикатора 
+        }
+
+        // сервис
+
+               /// <summary>
+        /// проверить условия на вход в позицию
+        /// </summary>
 
         /// <summary>
         /// вернуть название робота
@@ -33,16 +91,9 @@ namespace OsEngine.Robots
         /// </summary>
         public override void ShowIndividualSettingsDialog()
         {
-           
+
         }
 
-        /// <summary>
-        /// проверить условия на вход в позицию
-        /// </summary>
-        private void TryOpenPosition(List<Candle> candles)
-        {
-            decimal lastPrice = candles[candles.Count - 1].Close;
-            // проверка индикатора 
-        }
     }
-}// темт 
+   
+}
