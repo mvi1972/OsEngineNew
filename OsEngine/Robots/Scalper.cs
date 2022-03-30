bcf5d387d15e03ccb63cef2900e30bcbd4886fb1
mvi1972 <61858922+mvi1972@users.noreply.github.com>
2022-03-30 21:58:38 +0300

    создал метод расчета фазы роста

--- OsEngine/Robots/Scalper.cs
+++ OsEngine/Robots/Scalper.cs
@@ -11,11 +11,17 @@ namespace OsEngine.Robots
 {
     public class Scalper : BotPanel
     {
-        // публичные переменные
+        #region публичные переменные
+
         /// <summary>
         /// вкл/выкл для бумаги
         /// </summary>
         public bool IsOnPapir;
+
+        /// <summary>
+        /// вкл/выкл по хвостам
+        /// </summary>
+        public bool Tail;
 
         /// <summary>
         /// цена начала фазы роста
@@ -25,10 +31,11 @@ namespace OsEngine.Robots
         /// <summary>
         /// значение фазы роста
         /// </summary>
-        public bool PhaseGrowth;
+        public bool phaseGrowth;
 
-        // настройки на параметрах
-        private StrategyParameterInt CandleBack;
+        #endregion публичные переменные
+
+        #region настройки на параметрах
 
         /// <summary>
         /// вкл/выкл для робота
@@ -36,19 +43,35 @@ namespace OsEngine.Robots
         private StrategyParameterBool IsOn;
 
         /// <summary>
+        /// количество свечей зоны роста
+        /// </summary>
+        private StrategyParameterInt candleBack;
+
+        /// <summary>
+        /// процентная величина зоны роста
+        /// </summary>
+        private StrategyParameterDecimal growthPercent;
+
+        #endregion настройки на параметрах
+
+        /// <summary>
         /// вкладка через которую робот совершает торговлю
         /// </summary>
         private BotTabSimple _tab;
 
-        /// <summary>
-        /// конструктор
-        /// </summary>
+        #region конструктор
+
         public Scalper(string name, StartProgram startProgram) : base(name, startProgram)
         {
             TabCreate(BotTabType.Simple);
             _tab = TabsSimple[0];
             _tab.CandleFinishedEvent += _tab_CandleFinishedEvent;
+
+            candleBack = CreateParameter("Зона роста сколько свечей", 10, 5, 20, 1);
+            growthPercent = CreateParameter("Процент роста бумаги", 3m, 2, 10, 1);
         }
+
+        #endregion конструктор
 
         /// <summary>
         /// событие завершения новой свечи
@@ -57,22 +80,72 @@ namespace OsEngine.Robots
         {
         }
 
-        // логика
+        #region Логика
+
         /// <summary>
         /// расчет фазы роста
         /// </summary>
         private void СalculationPhaseGrowth(List<Candle> candles)
         {
-            /*
-             *  если тру выходим
-            взять значение открытия свечи Н баров назад
-            взять значение закрытия последней свечи
-            рассчитать заданное значение в заданных процентах
-            если совпадает
-            ставим в PhaseGrowth значение тру
-            записываем значение цены в PriceGrowthPhase
+            int canBack = candleBack.ValueInt;
 
-             */
+            decimal maxBodyClose = 0; //максимальное значение закрытия последней свечи
+            decimal minBodyOpen = decimal.MaxValue; //минимальное значение закрытия последней свечи
+            decimal maxCandlesHigh = 0;  //  максимальное значение хая свечи
+            decimal minCandelesLow = decimal.MaxValue; //минимальный лой
+
+            if (Tail == false) //если галочка по хвостам ложь считаем по телам
+            {
+                for (int i = candles.Count - 1; i > 0 && i > candles.Count - 1 - canBack; i--)
+                { // вычисляем значения открытия и закрытия свечей
+                    if (candles[i].Close > maxBodyClose)
+                    {
+                        maxBodyClose = candles[i].Close;
+                    }
+                    if (candles[i].Open < minBodyOpen)
+                    {
+                        minBodyOpen = candles[i].Open;
+                    }
+                    decimal move = maxBodyClose - minBodyOpen;
+                    if (move > 0)
+                    {
+                        decimal moveInPepcent = move / minBodyOpen * 100; // изменение значение в процентах
+                        if (moveInPepcent >= growthPercent.ValueDecimal)
+                        {
+                            /*  ставим в phaseGrowth значение тру
+                            записываем значение цены в PriceGrowthPhase */
+                            phaseGrowth = true;
+                        }
+                    }
+                    else phaseGrowth = false;
+                }
+            }
+            if (Tail == true) // расчет по свечам с хвостами
+            {
+                for (int i = candles.Count - 1; i > 0 && i > candles.Count - 1 - canBack; i--)
+                {   // вычисляем значения High и Low
+                    if (candles[i].High > maxCandlesHigh)
+                    {
+                        maxCandlesHigh = candles[i].High;
+                    }
+                    if (candles[i].Low < minCandelesLow)
+                    {
+                        minCandelesLow = candles[i].Low;
+                    }
+                    decimal move = maxCandlesHigh - minCandelesLow;
+                    if (move > 0)
+                    {
+                        decimal moveInPepcent = move / minCandelesLow * 100; // изменение значение в процентах
+                        if (moveInPepcent >= growthPercent.ValueDecimal)
+                        {
+                            /*  ставим в phaseGrowth значение тру
+                            записываем значение цены в PriceGrowthPhase */
+                            phaseGrowth = true;
+                        }
+                    }
+                    else phaseGrowth = false;
+                }
+            }
         }
 
         /// <summary>
@@ -80,11 +153,18 @@ namespace OsEngine.Robots
         /// </summary>
         private void TryOpenPosition(List<Candle> candles)
         {
+            СalculationPhaseGrowth(candles);
+            if (phaseGrowth == false)
+            {
+                return;
+            }
             decimal lastPrice = candles[candles.Count - 1].Close;
             // проверка индикатора
         }
 
-        // сервис
+        #endregion Логика
+
+        #region сервис
 
         /// <summary>
         /// проверить условия на вход в позицию
@@ -104,5 +184,7 @@ namespace OsEngine.Robots
         public override void ShowIndividualSettingsDialog()
         {
         }
+
+        #endregion сервис
     }
 }
