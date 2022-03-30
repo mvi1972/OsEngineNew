143371ae5834f14dde97030695e054ed0b168341
mvi1972 <61858922+mvi1972@users.noreply.github.com>
2022-03-30 13:13:29 +0300

    разделил на регионы

--- OsEngine/Robots/Scalper.cs
+++ OsEngine/Robots/Scalper.cs
@@ -7,27 +7,29 @@ using OsEngine.Market;
 using OsEngine.OsTrader.Panels;
 using OsEngine.OsTrader.Panels.Tab;
 
-
 namespace OsEngine.Robots
 {
     public class Scalper : BotPanel
     {
-        // публичные переменные 
+        // публичные переменные
         /// <summary>
         /// вкл/выкл для бумаги
         /// </summary>
         public bool IsOnPapir;
+
         /// <summary>
         /// цена начала фазы роста
         /// </summary>
         public decimal PriceGrowthPhase;
+
         /// <summary>
         /// значение фазы роста
         /// </summary>
         public bool PhaseGrowth;
 
-        // настройки на параметрах 
+        // настройки на параметрах
         private StrategyParameterInt CandleBack;
+
         /// <summary>
         /// вкл/выкл для робота
         /// </summary>
@@ -39,7 +41,7 @@ namespace OsEngine.Robots
         private BotTabSimple _tab;
 
         /// <summary>
-        /// конструктор 
+        /// конструктор
         /// </summary>
         public Scalper(string name, StartProgram startProgram) : base(name, startProgram)
         {
@@ -53,26 +55,24 @@ namespace OsEngine.Robots
         /// </summary>
         private void _tab_CandleFinishedEvent(List<Candle> candles)
         {
-            
         }
 
-        // логика 
+        // логика
         /// <summary>
         /// расчет фазы роста
         /// </summary>
         private void СalculationPhaseGrowth(List<Candle> candles)
         {
             /*
-             *  если тру выходим 
-            взять значение открытия свечи Н баров назад 
-            взять значение закрытия последней свечи 
-            рассчитать заданное значение в заданных процентах 
-            если совпадает 
+             *  если тру выходим
+            взять значение открытия свечи Н баров назад
+            взять значение закрытия последней свечи
+            рассчитать заданное значение в заданных процентах
+            если совпадает
             ставим в PhaseGrowth значение тру
             записываем значение цены в PriceGrowthPhase
 
              */
-
         }
 
         /// <summary>
@@ -81,7 +81,7 @@ namespace OsEngine.Robots
         private void TryOpenPosition(List<Candle> candles)
         {
             decimal lastPrice = candles[candles.Count - 1].Close;
-            // проверка индикатора 
+            // проверка индикатора
         }
 
         // сервис
@@ -99,13 +99,10 @@ namespace OsEngine.Robots
         }
 
         /// <summary>
-        /// показать окно индивидуальных настроек 
+        /// показать окно индивидуальных настроек
         /// </summary>
         public override void ShowIndividualSettingsDialog()
         {
-
         }
-
     }
-   
 }
